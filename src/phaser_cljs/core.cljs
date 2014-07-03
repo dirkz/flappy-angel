c324dc302509bbb45aecb1aaf15997cc8a031226
Dirk <me@dirkz.com>
2014-07-03 20:13:12 +0200

    renamed game into state to reflect its meaning

--- src/phaser_cljs/core.cljs
+++ src/phaser_cljs/core.cljs
@@ -1,26 +1,26 @@
 (ns phaser_cljs.core
   (:require [phaser.core]))
 
-(defn load-image [game key path]
-  (-> game .-load (.image key path)))
+(defn load-image [state key path]
+  (-> state .-load (.image key path)))
 
-(defn load-spritesheet [game key path a b]
-  (-> game .-load (.spritesheet key path a b)))
+(defn load-spritesheet [state key path a b]
+  (-> state .-load (.spritesheet key path a b)))
 
-(defn preload [game]
-  (load-image game "sky" "assets/sky.png")
-  (load-image game "ground" "assets/platform.png")
-  (load-image game "star" "assets/star.png")
-  (load-spritesheet game "dude" "assets/dude.png" 32 48))
+(defn preload [state]
+  (load-image state "sky" "assets/sky.png")
+  (load-image state "ground" "assets/platform.png")
+  (load-image state "star" "assets/star.png")
+  (load-spritesheet state "dude" "assets/dude.png" 32 48))
 
-(defn create [game]
-  (-> game .-add (.sprite 0 0 "star")))
+(defn create [state]
+  (-> state .-add (.sprite 0 0 "star")))
 
 (defn start []
   (let [width (.-innerWidth js/window)
         height (.-innerHeight js/window)
-        game (js/Phaser.Game. width height js/Phaser.AUTO ""
-                              (clj->js {:preload preload :create create}))]
-    (.log js/console game)))
+        state (js/Phaser.Game. width height js/Phaser.AUTO ""
+                               (clj->js {:preload preload :create create}))]
+    (.log js/console state)))
 
 (start)
